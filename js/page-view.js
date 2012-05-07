e6d719cccd4d508872948486aff4dd1fcc578455
Serj Ryabokon <ryabokon.serhiy@gmail.com>
2012-05-07 19:25:22 +0300

    added menu

--- js/page-view.js
+++ js/page-view.js
@@ -1,5 +1,44 @@
 PageView = {
+
    init: function() {
+      PageLoadedEvent.subscribe(this);
+   
       return this;
+   },
+   
+   onPageLoaded: function() {
+    
+     $('#menu-file').menu({ 
+        content: $('#menu-file').next().html(), 
+        flyOut: true
+     });
+
+     $('#menu-edit').menu({ 
+            content: $('#menu-edit').next().html(),
+            flyOut: true
+         });
+         
+
+     $('#menu-page-settings').menu({ 
+            content: $('#menu-page-settings').next().html(),
+            flyOut: true
+         });
+
+     $('#menu-templates').menu({ 
+            content: $('#menu-templates').next().html(), 
+            flyOut: true
+         });
+
+     $('#menu-preview-page').menu({ 
+            content: $('#menu-preview-page').next().html(), 
+            flyOut: true
+         });   
+
+     $('#menu-help').menu({ 
+            content: $('#menu-help').next().html(), 
+            flyOut: true
+         });
+		
    }
+   
 }.init();
