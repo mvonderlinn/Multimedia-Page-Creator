57ee8fe30aac4e6e752a6436a5d7d1938e249cc9
Serj Ryabokon <ryabokon.serhiy@gmail.com>
2012-05-11 17:26:12 +0300

    added text properties

--- js/controller/text-controller.js
+++ js/controller/text-controller.js
@@ -1,4 +1,6 @@
 TextController = {
+   activeElement: null,
+   
    init: function() {
       PageLoadedEvent.subscribe(this);
       CanvasContainerDropEvent.subscribe(this);
@@ -14,28 +16,60 @@ TextController = {
       });
       
       $(".mpc-tool-text").draggable();
+      
+      $("#mpc-text-ok-btn").on("click", function(){
+                  
+         TextController.activeElement.css("font-family", $("#mpc-text-font-family-sel").val() );
+         TextController.activeElement.css("font-size", $("#mpc-text-font-size-sel").val() +"px" );
+         TextController.activeElement.css("line-height", $("#mpc-text-font-size-sel").val() +"px" );
+         TextController.activeElement.css("color", "#" + $("#mpc-text-color-field").val() );
+         TextController.activeElement.css("text-align", $("#mpc-text-align-sel").val() );
+         TextController.activeElement.css("-moz-transform", "rotate(" + parseInt($("#mpc-text-rotate-deg").val()) + "deg)");
+         TextController.activeElement.css("-webkit-transform", "rotate(" + parseInt($("#mpc-text-rotate-deg").val()) + "deg)");
+         
+         $("#mpc-text-dlg").dialog( "close" );
+      });
    },
    
    onCanvasContainerDrop: function(domEl) {
    
-      if(domEl.hasClass("mpc-tool-text")) {
+      if(domEl.hasClass("mpc-tool-text") && !domEl.hasClass("ui-resizable") ) {
+      
+         
+         TextController.activeElement = domEl;
+
+         $("#mpc-text-dlg").dialog({modal: true});
+
          domEl.removeClass("mpc-tool");
          domEl.children(".mpc-caption").remove();
+         domEl.detach();
+         domEl.appendTo("#mpc-canvas-container");
+         
          domEl.css("line-height", "16px");
          domEl.css("overflow", "hidden");
+         domEl.css("text-shadow", "none");
+         
          domEl.resizable({
             ghost: false,
             grid: false,
             helper: false,
             handles: "n, e, s, w"
-            
          });
-         
+
          domEl.draggable({
             containment: $("#mpc-canvas-container")
          });
+
+         domEl.dblclick(function() {
+            $("#mpc-text-dlg").dialog({modal: true});
+         });
+         
+         $("#mpc-text-color-field").ColorPicker( {
+            onChange: function(col, inRgb) {
+               $("#mpc-text-color-field").val(inRgb);
+            
+            }});
       }   
-   
    }
    
 }.init();
