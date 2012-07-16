d10bb8b3ca3fa3b3738c06c77a898af46ccf243c
Serj Ryabokon <ryabokon.serhiy@gmail.com>
2012-07-16 20:48:49 +0300

    extracted to prototype shape controller builder obj

--- js/controller/rectangle-controller.js
+++ js/controller/rectangle-controller.js
@@ -1,136 +1,15 @@
-RectangleController = {
-
-   init: function() {
-      PageLoadedEvent.subscribe(this);
-      CanvasContainerDropEvent.subscribe(this);
-      CanvasElementSelectedEvent.subscribe(this);
-      DeleteKeyEvent.subscribe(this);
-      UpdateActiveElementEvent.subscribe(this);
+/**
+ * Controller for rectangle shapes
+ */
+RectangleController = new ShapeControllerBuilder("mpc-tool-rectangle").init();
+   
+   
+/**
+ * Creates new icon in toolset
+ */
+RectangleController.addNewIcon = function() {
       
-      return this;      
-   },
-   
-   onPageLoaded: function() {
-      ShapeController.onPageLoaded(".mpc-tool-rectangle");
-   },
-   
-   onCanvasContainerDrop: function(domEl) {
-      if ( domEl.hasClass("mpc-tool-rectangle") && domEl.hasClass( "mpc-tool" ) ) {
-         domEl.draggable({revert: false});
-         this.locateOnCanvas( domEl );
-         this.addDefaultAttrs( domEl );
-         PaintElementEvent.trigger( domEl );
-         this.addNewIcon();
-      }
-   },
-   
-   onCanvasElementSelected: function(domEl) {
-      if( $(domEl).hasClass( "mpc-tool-rectangle" ) ){
-         $(".mpc-active-el").resizable( "destroy" );
-         $(".mpc-active-el").removeClass( "mpc-active-el" );         
-         $(domEl).addClass( "mpc-active-el" );
-         this.enableResizing( $(domEl) );
-      }   
-   },
-   
-   onDeleteKey: function() {
-      $(".mpc-tool-rectangle.mpc-active-el").remove();   
-   },
-   
-   onUpdateActiveElement: function(properties) {   
-      if( $(".mpc-tool-rectangle.mpc-active-el").length ) {
-
-         var domEl = $( ".mpc-tool-rectangle.mpc-active-el" );
-
-         domEl.attr( "mpcIsStroked",    properties.mpcIsStroked );
-         domEl.attr( "mpcBorderWidth",  properties.mpcBorderWidth );
-         domEl.attr( "mpcIsFilled",     properties.mpcIsFilled );
-         domEl.attr( "mpcFillColor",    properties.mpcFillColor );
-         domEl.attr( "mpcBorderColor",  properties.mpcBorderColor );
-
-         PaintElementEvent.trigger(domEl);
-      }
-   },
-   
-   locateOnCanvas: function(domEl) {
-     domEl.removeClass( "mpc-tool" );
-     
-     domEl.children( ".mpc-caption" ).remove();
-
-     domEl.detach();
-     domEl.appendTo("#mpc-canvas-container");
-     
-     $(".mpc-active-el").resizable( "destroy" );
-     $(".mpc-active-el").removeClass("mpc-active-el");
-     
-     $(domEl).addClass("mpc-active-el");
-     
-     /**
-      * Positioning
-      */
-     var topUpdated = domEl.position().top - $("#mpc-canvas-wrapper").position().top;
-     var leftUpdated = domEl.position().left - $("#mpc-canvas-wrapper").position().left;
-
-     domEl.css("top", topUpdated + "px");
-     domEl.css("left", leftUpdated + "px");
-     domEl.css("overflow", "hidden");
-     
-     /**
-      * Adding resizable and draggable properties
-      */
-     this.enableResizing(domEl);
-
-     domEl.draggable({
-        containment: "#mpc-canvas-container",
-        start: function() {
-            CanvasElementSelectedEvent.trigger(this);
-        }
-     });
-
-     /**
-      * chain of responsibility, kind of ..
-      */
-      domEl.click(function() {
-         CanvasElementSelectedEvent.trigger(this);
-      });
-
-      domEl.dblclick(function() {
-         CanvasElementSelectedEvent.trigger(this);
-         ModifyCanvasElementEvent.trigger(this);
-      });
-   },
-   
-   enableResizing: function(domEl) {
-     domEl.resizable({
-        disabled: false,
-        handles: "n, e, s, w, ne, se, sw, nw ",
-        containment: "#mpc-canvas-container",
-        resize: function(event, ui) {
-           PaintElementEvent.trigger( this );
-        }
-     });   
-   },
-      
-   /**
-    * Initializes default properties
-    * of newly created canvas graphical object
-    */
-   addDefaultAttrs: function( domEl ) {
-      domEl.attr("mpcIsStroked", "true");
-      domEl.attr("mpcBorderWidth", "1.0");
-      domEl.attr("mpcIsFilled", "false");
-      domEl.attr("mpcFillColor", "#000000");
-      domEl.attr("mpcBorderColor", "#000000");
-   },
-   
-   /**
-    * Creates new icon in toolset
-    * after creating new 
-    */
-   addNewIcon: function() {
-      $('<div class="mpc-tool mpc-tool-rectangle" mpcIsStroked="true" mpcBorderWidth="1" mpcIsFilled="false" mpcFillColor="#ffffff" mpcBorderColor="#000000"><canvas></canvas><div class="mpc-caption">rectangle</div></div>').appendTo("#mpc-tools");
-      $('.mpc-tool.mpc-tool-rectangle').draggable({revert: true});
-      PaintElementEvent.trigger($('.mpc-tool.mpc-tool-rectangle'));
-   }   
-   
-}.init();
+   $('<div class="mpc-tool mpc-tool-rectangle" mpcIsStroked="true" mpcBorderWidth="1" mpcIsFilled="false" mpcFillColor="#ffffff" mpcBorderColor="#000000"><canvas></canvas><div class="mpc-caption">rectangle</div></div>').appendTo("#mpc-tools");
+   $('.mpc-tool.mpc-tool-rectangle').draggable({revert: true});
+   PaintElementEvent.trigger($('.mpc-tool.mpc-tool-rectangle'));      
+};
--- js/controller/shape-controller.js
+++ js/controller/shape-controller.js
@@ -1,15 +1,150 @@
-ShapeController = {
+/**
+ * General shape controller 
+ */
+ShapeControllerBuilder = function(shapeClass) {
+   this.shapeClass = shapeClass;
+};
 
-   onPageLoaded: function(shapeClass) {
-      $(shapeClass).each(function() {
+ShapeControllerBuilder.prototype.init = function() {
+   PageLoadedEvent.subscribe(this);
+   CanvasContainerDropEvent.subscribe(this);
+   CanvasElementSelectedEvent.subscribe(this);
+   DeleteKeyEvent.subscribe(this);
+   UpdateActiveElementEvent.subscribe(this);
+   
+   return this; 
+};
+
+/**
+ * Handler being called right after openning MPC
+ */
+ShapeControllerBuilder.prototype.onPageLoaded = function() {
+   $("." + this.shapeClass).each(function() {
+      PaintElementEvent.trigger( this );
+   });
+   
+   $("." + this.shapeClass).draggable({
+      revert: true
+   });    
+};
+
+/**
+ * Handler for dropping a tool on canvas
+ */
+ShapeControllerBuilder.prototype.onCanvasContainerDrop = function(domEl) {
+   if ( domEl.hasClass(this.shapeClass) && domEl.hasClass( "mpc-tool" ) ) {
+      domEl.draggable({revert: false});
+      this.locateOnCanvas( domEl );
+      this.addDefaultAttrs( domEl );
+      PaintElementEvent.trigger( domEl );
+      this.addNewIcon();
+   }
+};
+
+/**
+ * Handler for selecting shape
+ */
+ShapeControllerBuilder.prototype.onCanvasElementSelected = function( domEl ) {
+   if( $(domEl).hasClass( this.shapeClass ) ){
+      $(".mpc-active-el").resizable( "destroy" );
+      $(".mpc-active-el").removeClass( "mpc-active-el" );         
+      $(domEl).addClass( "mpc-active-el" );
+      this.enableResizing( $(domEl) );
+   }
+};
+
+/**
+ * Handler for selecting shape
+ */
+ShapeControllerBuilder.prototype.onDeleteKey = function() {
+   $("." + this.shapeClass + ".mpc-active-el").remove();
+};
+
+ShapeControllerBuilder.prototype.onUpdateActiveElement = function(properties) {
+   if( $("." + this.shapeClass + ".mpc-active-el").length ) {
+      var domEl = $( "." + this.shapeClass + ".mpc-active-el" );
+
+      domEl.attr( "mpcIsStroked",    properties.mpcIsStroked );
+      domEl.attr( "mpcBorderWidth",  properties.mpcBorderWidth );
+      domEl.attr( "mpcIsFilled",     properties.mpcIsFilled );
+      domEl.attr( "mpcFillColor",    properties.mpcFillColor );
+      domEl.attr( "mpcBorderColor",  properties.mpcBorderColor );
+
+      PaintElementEvent.trigger(domEl);
+   }
+};
+
+/**
+ * Transforms a tool icon into graphical object
+ * on canvas
+ */
+ShapeControllerBuilder.prototype.locateOnCanvas = function(domEl) {
+   domEl.removeClass( "mpc-tool" );
+   
+   domEl.children( ".mpc-caption" ).remove();
+
+   domEl.detach();
+   domEl.appendTo("#mpc-canvas-container");
+   
+   $(".mpc-active-el").resizable( "destroy" );
+   $(".mpc-active-el").removeClass("mpc-active-el");
+   
+   $(domEl).addClass("mpc-active-el");
+   
+   /**
+    * Positioning
+    */
+   var topUpdated = domEl.position().top - $("#mpc-canvas-wrapper").position().top;
+   var leftUpdated = domEl.position().left - $("#mpc-canvas-wrapper").position().left;
+
+   domEl.css("top", topUpdated + "px");
+   domEl.css("left", leftUpdated + "px");
+   domEl.css("overflow", "hidden");
+   
+   /**
+    * Adding resizable and draggable properties
+    */
+   this.enableResizing(domEl);
+
+   domEl.draggable({
+      containment: "#mpc-canvas-container",
+      start: function() {
+          CanvasElementSelectedEvent.trigger(this);
+      }
+   });
+
+   /**
+    * chain of responsibility, kind of ..
+    */
+    domEl.click(function() {
+       CanvasElementSelectedEvent.trigger(this);
+    });
+
+    domEl.dblclick(function() {
+       CanvasElementSelectedEvent.trigger(this);
+       ModifyCanvasElementEvent.trigger(this);
+    });
+};
+
+ShapeControllerBuilder.prototype.enableResizing = function(domEl) {
+   domEl.resizable({
+      disabled: false,
+      handles: "n, e, s, w, ne, se, sw, nw",
+      containment: "#mpc-canvas-container",
+      resize: function(event, ui) {
          PaintElementEvent.trigger( this );
-      });
+      }
+   });   
+};
       
-      $(shapeClass).draggable({
-         revert: true
-      });    
-   },
-   
-   
-   
+/**
+ * Initializes default properties
+ * of newly created canvas graphical object
+ */
+ShapeControllerBuilder.prototype.addDefaultAttrs = function( domEl ) {
+   domEl.attr("mpcIsStroked", "true");
+   domEl.attr("mpcBorderWidth", "1.0");
+   domEl.attr("mpcIsFilled", "false");
+   domEl.attr("mpcFillColor", "#000000");
+   domEl.attr("mpcBorderColor", "#000000");
 };
