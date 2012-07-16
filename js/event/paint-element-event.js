00cf4054fab0782baf1f9d1c064c36fa2a502b13
Serj Ryabokon <ryabokon.serhiy@gmail.com>
2012-07-16 19:17:03 +0300

    paint element event

---
+++ js/event/paint-element-event.js
@@ -0,0 +1,51 @@
+PaintElementEvent = {
+
+   listeners: [],
+
+   onPageLoaded: function() {
+      $(document).keypress(function(ev) {         
+         var delKeyCode = 46;
+         var k = ev.which ? ev.which : ev.keyCode;
+         if(k === delKeyCode) {
+            DeleteKeyEvent.trigger();
+         }
+      });
+   },
+
+   /**
+    * ChainOfResponsibility
+    */
+   trigger: function(domEl) {
+      for(var oKey in this.listeners) {
+         var obj = this.listeners[oKey];
+         if("onPaintElement" in obj) {
+            obj.onPaintElement(domEl);
+         }
+      }
+   },
+
+   subscribe: function(obj) {
+      this.listeners.push(obj);
+   },
+
+   unsubscribe: function(obj) {
+      /**
+      * searching for an index of the object passed
+      */
+      var foundIndex = -1;
+      for(var index = 0; index < this.listeners.length; index++) {
+         var iteratedObj = this.listeners[index];
+         if(obj == iteratedObj) {
+            foundIndex = index;
+         }
+      }
+      
+      if(foundIndex >= 0) {
+         /**
+         * removing found listener
+         */
+         this.listeners = this.listeners.splice(foundIndex + 1, 1);
+      }
+   }
+
+}.init();
