7e6b19ba68ca3781517dbd62b145ee645fd859ec
Serj Ryabokon <ryabokon.serhiy@gmail.com>
2012-07-24 02:45:55 +0300

    images

--- js/view/dialog/image-dialog.js
+++ js/view/dialog/image-dialog.js
@@ -1,15 +1,51 @@
-DiamondDialog = {
-   
+ImageDialog = {
+   dlgLabel: "Image",
+
    init: function() {
+      PageLoadedEvent.subscribe(this);
       ModifyCanvasElementEvent.subscribe(this);
-      
       return this;
+
+   },
+
+   onModifyCanvasElement: function(domEl) {
+      if($(domEl).hasClass("mpc-tool-image")) {      
+         this.show( domEl );
+      }
+   },
+
+   onPageLoaded: function() {
+      $("#mpc-image-ok-btn").click(function() {
+         $("#mpc-image-dlg").dialog("close");
+         ImageDialog.applyProperties();
+      });
    },
    
-   onModifyCanvasElement: function(domEl) {
-      if($(domEl).hasClass("mpc-tool-diamond")) {
-         ShapeDialog.show( domEl, "Diamond" );
+   /**
+    * Handler for
+    */
+   show: function(domEl) {
+      domEl = $(domEl);
+      
+      $("#mpc-image-dlg").dialog({modal:true, title: this.dlgLabel});
+   },
+   
+   applyProperties: function() {
+      var imagDataURLs = [];
+
+      var files = document.getElementById("mpc-image-dlg-file").files;
+
+      for(var index in files) {
+         var f = files[index];
+         console.log(f);
       }
-   }
+      
+
+      var properties = {
+                        mpcImages: imagDataURLs
+                       };
+
+      UpdateActiveElementEvent.trigger(properties);
+   }   
    
 }.init();
